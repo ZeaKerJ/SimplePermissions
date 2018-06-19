5cee9a9bbc65acac4a95a339888dcd7b6c469ae6
ZeaKerJ <38295338+ZeaKerJ@users.noreply.github.com>
2018-06-19 13:31:49 -0500

    Update Permission.java
    Hi, i think the ACCESS_NETWORK_STATE is a good update of permissions, i need include in your list... please :)

--- simplepermission/src/main/java/com/luseen/simplepermission/permissions/Permission.java
+++ simplepermission/src/main/java/com/luseen/simplepermission/permissions/Permission.java
@@ -30,6 +30,7 @@ public enum Permission {
     COARSE_LOCATION(Manifest.permission.ACCESS_COARSE_LOCATION),
     READ_EXTERNAL_STORAGE(Manifest.permission.READ_EXTERNAL_STORAGE),
     WRITE_EXTERNAL_STORAGE(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    ACCESS_NETWORK_STATE(Manifest.permission.ACCESS_NETWORK_STATE);
 
     String permission;
 
